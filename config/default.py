6484cc7f5e7918a363d578a89283e94771987d8a
Steffen Maass <steffen.maass@gatech.edu>
2017-10-03 23:54:57 -0400

    Config for a two-socket, 16-core machine

--- config/default.py
+++ config/default.py
@@ -43,7 +43,7 @@ RBIN_MOSAIC = join(BUILD_ROOT, "Release-x86_64/lib/core/mosaic")
 SG_NUM_HASH_DIRS = 512          # #hash dirs
 SG_PORT = 5000                  # The base port for Mosaic, is used to connect to the Xeon Phi.
 SG_ALGORITHM = "pagerank"       # The default algorithm.
-SG_NMIC = 1                     # The default number of edge-engines (on the Xeon Phi or Host) to use.
+SG_NMIC = 2                     # The default number of edge-engines (on the Xeon Phi or Host) to use.
 SG_RUN_ON_MIC = 0               # Whether to instantiate on host or Xeon Phi.
 SG_PRINT_ONLY = False           # Whether to only print the commands or also execute them.
 SG_DATASET = "twitter-small"    # The default dataset.
@@ -62,12 +62,12 @@ SG_PATH_PERF_EVENTS = join(DATA_ROOT, "perf-events") # Where to output the JSON 
 
 # vertex-engine config
 SG_MAX_ITERATIONS = 20  # Number of iterations.
-SG_NAPPLIER = 48        # Number of appliers, global,
-SG_NGLOBALREDUCER = 8   # Number of global reducers.
+SG_NAPPLIER = 32        # Number of appliers, global,
+SG_NGLOBALREDUCER = 2   # Number of global reducers.
 SG_NGLOBALFETCHER = 8   # Number of global fetchers.
 SG_NREDUCER = 2         # Number of reducers, instantiated PER edge-engine, local. Total number of reducers: SG_NMIC * SG_NREDUCER
-SG_NFETCHER = 3         # Number of fetchers, instantiated PER edge-engine, local. Total number of reducers: SG_NMIC * SG_NFETCHER
-SG_NINDEXREADER = 3     # Number of index readers PER edge-engine.
+SG_NFETCHER = 2         # Number of fetchers, instantiated PER edge-engine, local. Total number of reducers: SG_NMIC * SG_NFETCHER
+SG_NINDEXREADER = 1     # Number of index readers PER edge-engine.
 SG_ENABLE_LOG = False   # Outputs the vertex values per iteration into a file, separating values by '\n'.
 SG_ENABLE_FAULT_TOLERANCE = False # Whether to enable the fault tolerance mode, flushing the vertex values to disk asynchronously per iteration.
 # Benchmark options that allow enabling/disabling specific parts of the system.
@@ -86,8 +86,8 @@ SG_GLOBAL_FETCHER_MODE = "Active"
 SG_EDGE_ENGINE_TO_MIC = [ 0, 1, 2, 3 ] 
 
 # Edge-engine config
-SG_NREADER = 2     # When running on the host: Number of readers to instantiate to read the tiles into memory.
-SG_NPROCESSOR = 3  # When running on the host: Number of tile processors to actually process the tiles with.
+SG_NREADER = 1     # When running on the host: Number of readers to instantiate to read the tiles into memory.
+SG_NPROCESSOR = 4  # When running on the host: Number of tile processors to actually process the tiles with.
 
 SG_NREADER_MIC = 2    # When running on the Xeon Phi: Number of readers to instantiate to read the tiles into memory.
 SG_NPROCESSOR_MIC = 3 # When running on the Xeon Phi: Number of tile processors to actually process the tiles with.
@@ -95,10 +95,10 @@ SG_NPROCESSOR_MIC = 3 # When running on the Xeon Phi: Number of tile processors 
 SG_COUNT_FOLLOWERS = 1; # The number of hyperthreads to use per tile processor.
 
 # Ring buffer size configuration for the edge-engine.
-SG_RB_SIZE_PROCESSED = 1 * GB             # The size of the ringbuffer to save the result into.
+SG_RB_SIZE_PROCESSED = 8 * GB             # The size of the ringbuffer to save the result into.
 SG_RB_SIZE_READ_TILES = 1 * GB            # The size of the ringbuffer to read tiles from disk into.
 SG_RB_SIZE_READ_TILES_IN_MEMORY = 32 * GB # The size of the ringbuffer to read tiles from disk into when running in the in-memory mode AND on the host.
-SG_RB_SIZE_HOST_TILES = 1 * GB            # The size of the ringbuffer to receive the tile information from the host (fetcher) into.
+SG_RB_SIZE_HOST_TILES = 8 * GB            # The size of the ringbuffer to receive the tile information from the host (fetcher) into.
 
 # Benchmarking modes.
 # Whether to activate the tile processor or not, options are: Active, Noop.
@@ -111,7 +111,10 @@ SG_TILE_PROCESSOR_INPUT_MODE = "VertexFetcher"
 SG_TILE_PROCESSOR_OUTPUT_MODE = "VertexReducer"
 
 # datapath
-SG_DATAPATH_EDGE_ENGINE = {0: join(DATA_ROOT, "datapath", "nvme0")}
+SG_DATAPATH_EDGE_ENGINE = {
+        0: join(DATA_ROOT, "datapath", "nvme0"),
+        1: join(DATA_ROOT, "datapath", "nvme1")
+        }
 SG_DATAPATH_VERTEX_ENGINE = SG_DATAPATH_EDGE_ENGINE
 
 # grc config
